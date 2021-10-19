add90bf77b73b79f40fba9500f319debbe8b25ac
hefujie <hefujie@msn.cn>
2021-10-19 21:50:49 +0800

    beforeDatabase

--- EmployeeDB/Employee.cpp
+++ EmployeeDB/Employee.cpp
@@ -1,1 +1,78 @@
 #include "Employee.h"
+#include <string>
+#include <iostream>
+Records::Employee::Employee(const string& firstName, const string& lastName):mFirstName(firstName), mLastName(lastName)
+{
+}
+
+void Records::Employee::promote(int raiseAmount)
+{
+	setSalary(getSalary() + raiseAmount);
+}
+
+void Records::Employee::demote(int demeritAmount)
+{
+	setSalary(getSalary() - demeritAmount);
+}
+
+void Records::Employee::hire()
+{
+	mHired = true;
+}
+
+void Records::Employee::fire()
+{
+	mHired = false;
+}
+
+void Records::Employee::display() const
+{
+	cout << "Employee: " << getLastName() << ", " << getFirstName() << endl;
+	cout << "-------------------------" << endl;
+	cout << (isHired() ? "Current Employee" : "Former Employee") << endl;
+	cout << "Employee Number: " << getEmployeeNumber() << endl;
+	cout << "Salary: $" << getSalary() << endl;
+	cout << endl;
+}
+
+void Records::Employee::setFirstName(const string& firstName)
+{
+	mFirstName = firstName;
+}
+
+const string& Records::Employee::getFirstName() const
+{
+	return mFirstName;
+}
+
+void Records::Employee::setLastName(const string& lastName)
+{
+}
+
+const string& Records::Employee::getLastName() const
+{
+	return mLastName;
+}
+
+void Records::Employee::setEmployeeNumber(int employeeNumber)
+{
+}
+
+int Records::Employee::getEmployeeNumber() const
+{
+	return 0;
+}
+
+void Records::Employee::setSalary(int newSalary)
+{
+}
+
+int Records::Employee::getSalary() const
+{
+	return 0;
+}
+
+bool Records::Employee::isHired() const
+{
+	return false;
+}
--- EmployeeDB/Employee.h
+++ EmployeeDB/Employee.h
@@ -1,5 +1,40 @@
 #pragma once
-class Employee
+#include <string>
+using namespace std;
+namespace Records
 {
-};
+	const int kDefaultStartingSalary = 30000;
+	class Employee
+	{
+	public:
+		Employee() = default;
+		Employee(const string& firstName, const string& lastName);
+
+		void promote(int raiseAmount = 1000);
+		void demote(int demeritAmount = 1000);
+		void hire();
+		void fire();
+		void display() const;	//前面使用const 表示返回值为const，后面加 const表示函数不可以修改class的成员
+
+		void setFirstName(const string& firstName);
+		const string& getFirstName() const;
+
+		void setLastName(const string& lastName);
+		const string& getLastName() const;
+
+		void setEmployeeNumber(int employeeNumber);
+		int getEmployeeNumber() const;
+
+		void setSalary(int newSalary);
+		int getSalary() const;
+
+		bool isHired() const;
+	private:
+		string mFirstName;
+		string mLastName;
+		int mEmployeeNumber = -1;
+		int mSalary = kDefaultStartingSalary;
+		bool mHired = false;
+	};
+}
 
